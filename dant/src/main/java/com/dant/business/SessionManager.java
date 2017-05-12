84ed3de7ef843d2da68247556c8e2a0a3c15dce2
Hephixor <wsergeant.t@gmail.com>
2017-05-12 16:37:22 +0200

    Transcoders

--- dant/src/main/java/com/dant/business/SessionManager.java
+++ dant/src/main/java/com/dant/business/SessionManager.java
@@ -1,5 +1,10 @@
 package com.dant.business;
 
+import net.spy.memcached.CachedData;
+import net.spy.memcached.transcoders.BaseSerializingTranscoder;
+import net.spy.memcached.transcoders.SerializingTranscoder;
+import net.spy.memcached.transcoders.Transcoder;
+
 import com.dant.dao.MemcacheDAO;
 import com.dant.entity.Session;
 
@@ -14,12 +19,17 @@ public class SessionManager {
 	}
 
 	public void setSession(Session s){
-		dao.set(s.getIdUser(), s);
+		//dao.set(s.getIdUser(), s);
 	}
 
 	public void storeSession(Session s){
-		dao.set(s.getIdUser(),s);
+		dao.setSessionByIdUser(s.getIdUser(),s);
 	}
 	
+	public void testTranscoder(Object str){
+		SerializingTranscoder transcoder = new SerializingTranscoder();
+		System.out.println(transcoder.encode(str));
+		System.out.println(transcoder.toString());
+	}
 	
 }
