d482057c3c8262070f63d1e97816bd89ad3b63bc
Foxxx07 <florent.dijx@gmail.com>
2017-05-20 13:26:56 +0200

    clean

---
+++ iMapContacts/iMapContacts/Connexion.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  iMapContacts
+//
+//  Created by m2sar on 27/04/17.
+//  Copyright © 2017 m2sar. All rights reserved.
+//
+
+import UIKit
+
+class Connexion: UIViewController {
+
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func sendConnexion() -> Bool{
+     
+        var urlComponents = URLComponents()
+        
+        guard let mail = email.text, let pass = password.text else {return false}
+        guard mail.characters.count >= 6, pass.characters.count >= 4 else { return false } // Handle error todo
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "email" , value : email.text!.addingPercentEncoding(withAllowedCharacters: CharacterSet.alphanumerics)),
+            URLQueryItem(name: "password", value : password.text!.addingPercentEncoding(withAllowedCharacters: CharacterSet.alphanumerics))
+            ]
+        
+        let dataTask =  UrlUtils().sendToServ(httpMethod: HTTPMETHOD.POST, collection: USER.ME.rawValue, urlComponents: urlComponents)
+
+        let ableToConnect : Bool = true
+        //TODO : Faire un check des données reçu puis set le bool en fonction de si la connexion est possible ou non
+        
+        if (ableToConnect) {
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    @IBAction func seConnecter(_ sender: UIButton) {
+        let ableToConect: Bool = sendConnexion()
+        
+        if (ableToConect) {
+            self.performSegue(withIdentifier: "acceuil", sender: self)
+        }
+        else {
+            AlertView().showAlertView(targetVC: self, title: "Login ou mot de passe incorrect", message: "")
+        }
+    }
+    
+    @IBAction func signUp(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "inscrire", sender: self)
+    }
+    
+}
+
+
---
+++ iMapContacts/iMapContacts/ListeAmis.swift
@@ -0,0 +1,78 @@
+//
+//  Contacts.swift
+//  iMapContacts
+//
+//  Created by m2sar on 03/05/17.
+//  Copyright © 2017 m2sar. All rights reserved.
+//
+
+
+import UIKit
+class Contacts: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var contactList: [Amis] = []
+    
+    func createContact() {
+        let contactA = Amis(n: "contact A", f: 1)
+        let contactB = Amis(n: "contact B", f: 1)
+        contactList.append(contactA)
+        contactList.append(contactB)
+    }
+
+    
+    let cellReuseIdentifier = "cell"
+    
+
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createContact()
+        // It is possible to do the following three things in the Interface Builder
+        // rather than in code if you prefer.
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.contactList.count
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        
+        cell.textLabel?.text = self.contactList[indexPath.row].name
+        
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+    
+    // this method handles row deletion
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            // remove the item from the data model
+            contactList.remove(at: indexPath.row)
+            
+            // delete the table view row
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        } else if editingStyle == .insert {
+            // Not used in our example, but if you were adding a new row, this is where you would do it.
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Supprimer"
+    }
+   
+}
