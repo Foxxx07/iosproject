01f2fa4f4ca5a5a96203fffdda569946676c3d27
Foxxx07 <florent.dijx@gmail.com>
2017-05-20 13:33:13 +0200

    add clean: iMapContacts

---
+++ iMapContacts/iMapContacts/Conf.swift
@@ -0,0 +1,14 @@
+//
+//  Collection.swift
+//  iMapContacts
+//
+//  Created by m2sar on 04/05/17.
+//  Copyright © 2017 m2sar. All rights reserved.
+//
+
+import Foundation
+
+enum CONF : String {
+  case CONF = ""
+}
+
---
+++ iMapContacts/iMapContacts/SearchContact.swift
@@ -0,0 +1,18 @@
+//
+//  SearchContact.swift
+//  iMapContacts
+//
+//  Created by m2sar on 11/05/17.
+//  Copyright © 2017 m2sar. All rights reserved.
+//
+
+import UIKit
+
+class SearchContact: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("SearchContact")
+    }
+    
+    
+}
