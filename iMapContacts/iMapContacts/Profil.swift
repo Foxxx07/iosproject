5cb2445000de2ae48307547782f466adea1c110f
Ksujithan <skr-@hotmail.fr>
2017-05-29 18:58:00 +0200

    profil class update

--- iMapContacts/iMapContacts/Profil.swift
+++ iMapContacts/iMapContacts/Profil.swift
@@ -8,9 +8,17 @@
 
 import UIKit
 
-class Profil: UIViewController {
+class Profil: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
+    @IBOutlet weak var profilePicture: UIImageView!
     @IBOutlet weak var displayPosition: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        let defaults = UserDefaults.standard
+//        let token = defaults.string(forKey: "profilePicture")
+    }
+    
     
     @IBAction func onOffPosition(_ sender: AnyObject) {
         if (displayPosition.isOn) {
@@ -20,6 +28,40 @@ class Profil: UIViewController {
             //
         }
     }
+    
+    @IBAction func changeMail(_ sender: AnyObject) { 
+    }
+    
+    @IBAction func changePassword(_ sender: AnyObject) {
+    }
+    
+    @IBAction func selectPicture(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title:nil, message:nil, preferredStyle:
+            .actionSheet)
+        self.present(actionSheet, animated: true, completion: nil)
+        
+        
+        actionSheet.addAction(UIAlertAction(title:"Choisir une photo", style: .default, handler: { (action: UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        profilePicture.image = image
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
     
     
 }
