bb6b2e779ccf537d72a6812a69b5c17a17a06710
lessonsharing <maiuri.gaetan@gmail.com>
2017-06-14 09:35:57 +0200

    update: src/sql/sp_UpdateUser.sql

--- src/sql/sp_UpdateUser.sql
+++ src/sql/sp_UpdateUser.sql
@@ -9,22 +9,29 @@ MODIFIES SQL DATA
 SQL SECURITY DEFINER
 COMMENT 'Modifie les méta-données d\'un utilisateur'
 BEGIN
+	DECLARE EXIT HANDLER FOR SQLWARNING, SQLEXCEPTION
+		BEGIN
+			ROLLBACK;
+		END;
+
 	IF EXISTS(SELECT 1 FROM `users` WHERE `key` = u_key) THEN
-		IF (u_fname IS NOT NULL AND 0 < LENGTH(u_fname)) THEN
-			UPDATE `users` SET `fname` = u_fname WHERE `key` = u_key;
-		END IF;
+		START TRANSACTION;
+			IF (u_fname IS NOT NULL AND 0 < LENGTH(u_fname)) THEN
+				UPDATE `users` SET `fname` = u_fname WHERE `key` = u_key;
+			END IF;
 
-		IF (u_lname IS NOT NULL AND 0 < LENGTH(u_lname)) THEN
-			UPDATE `users` SET `lname` = u_lname WHERE `key` = u_key;
-		END IF;
+			IF (u_lname IS NOT NULL AND 0 < LENGTH(u_lname)) THEN
+				UPDATE `users` SET `lname` = u_lname WHERE `key` = u_key;
+			END IF;
 
-		IF (u_email IS NOT NULL AND RPAD(0x00, 254, 0x00) <> RPAD(u_email, 254, 0x00)) THEN
-			UPDATE `users` SET `email` = u_email WHERE `key` = u_key;
-		END IF;
+			IF (u_email IS NOT NULL AND RPAD(0x00, 254, 0x00) <> RPAD(u_email, 254, 0x00)) THEN
+				UPDATE `users` SET `email` = u_email WHERE `key` = u_key;
+			END IF;
 
-		IF (u_password IS NOT NULL AND RPAD(0x00, 32, 0x00) <> u_password) THEN
-			UPDATE `users` SET `password` = u_password WHERE `key` = u_key;
-		END IF;
+			IF (u_password IS NOT NULL AND RPAD(0x00, 32, 0x00) <> u_password) THEN
+				UPDATE `users` SET `password` = u_password WHERE `key` = u_key;
+			END IF;
+		COMMIT;
 	END IF;
 END;
 |
